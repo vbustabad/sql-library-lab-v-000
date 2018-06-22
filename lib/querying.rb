a334fc3d1527b3286a7583b64afae6447f7de5e4
Veronica Bustabad <vbustabad@gmail.com>
2018-06-22 21:27:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,6 +42,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name,
+  "SELECT characters.name, COUNT(character_id)
+  FROM characters
+  INNER JOIN character_books
+  ON characters.id = character_books.character_id
+  ORDER BY COUNT(*) DESC
   ";
 end
